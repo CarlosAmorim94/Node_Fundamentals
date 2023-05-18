5702f5efc9d56148f85a9cc4e252b36d5585bbd5
carlos <carlosa@xgrow.com>
2023-05-18 20:52:59 -0300

    Feat: Salvando usuários na memoria

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
+
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Carlos',
+      email: 'carlos.av.amorim@gmail.com'
+    })
+
+    return res.end('User created')
+  }
 
   return res.end('Hello World');
 });
