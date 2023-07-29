c972ce4c766694f40c882b7aaa4666dfda18ca8f
carlos <carlosa@xgrow.com>
2023-07-29 18:05:27 -0300

    Feat: Fix server response

--- src/server.js
+++ src/server.js
@@ -1,26 +1,27 @@
-import http from 'node:http';
+import http from "node:http";
 
 const users = [];
 
 const server = http.createServer((req, res) => {
-
   const { method, url } = req;
 
-  if (method === 'GET' && url === '/users') {
-    return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if (method === 'POST' && url === '/users') {
+  if (method === "POST" && url === "/users") {
     users.push({
       id: 1,
-      name: 'Carlos',
-      email: 'carlos.av.amorim@gmail.com'
-    })
+      name: "Carlos",
+      email: "carlos.av.amorim@gmail.com",
+    });
 
-    return res.status(201).json(users)
+    return res.writeHead(201).end();
   }
 
-  return res.writeHead(404).end('');
+  return res.writeHead(404).end();
 });
 
-server.listen(3333)
+server.listen(3333);
