6f938024c3b29be7979d06ecaac417e06816d612
carlos <carlosa@xgrow.com>
2023-08-02 20:36:39 -0300

    Feat: Add private prop in database

--- src/database.js
+++ src/database.js
@@ -1,16 +1,17 @@
 export class Database {
-  database = {};
+  // o # na frente do nome da propriedade indica que ela é privada
+  #database = {};
 
   select(table) {
-    const data = this.database[table] ?? [];
+    const data = this.#database[table] ?? [];
     return data;
   }
 
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
-      this.database[table].push(data);
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
     } else {
-      this.database[table] = [data];
+      this.#database[table] = [data];
     }
 
     return data;
